60fa7f3e115bca1ef269fbf03159cdcf49295048
Pascal Querner <pascal.querner@mscg.de>
2013-12-04 20:48:36 +0100

    removed unneeded files almost done?!

--- index.php
+++ index.php
@@ -6,55 +6,68 @@
     </head>
     <body>
         <?php
-        // Get all the HTML files from within the 2013 folder. (07 and 09)
+        // Get all the HTML files from within IT Essentials only!
         $filetypes = array("html");
-        $path = './html/html/aiubproblemsolver.blogspot.de/2013/';
+        $path = './ciscoanswers/ccna.smarthand.ro/';
         $di = new RecursiveDirectoryIterator($path);
         foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
             $filetype = pathinfo($file, PATHINFO_EXTENSION);
             if (in_array(strtolower($filetype), $filetypes)) {
-                $files[] = (string) $file;
+                if (strstr($file, "it-essentials-chapter"))
+                    $files[] = (string) $file;
             }
         }
-
         // Now get me my information!
         foreach ($files as $file) {
-            $folderName = explode('2013', $file);
-            $stringToSeachIn = $folderName[1];
-            $stringToSeachFor = '\\';
-            $pos = strpos($stringToSeachIn, $stringToSeachFor, 1);
-            $rawFolderName = substr($stringToSeachIn, 1, $pos);
-            $rawFolderName = str_replace("\\", "/", $rawFolderName);
-            $rawFileName = substr($stringToSeachIn, $pos + 1);
             $dom = new DOMDocument();
-            @$dom->loadHTMLFile($path . $rawFolderName . $rawFileName);
+            @$dom->loadHTMLFile($file);
             /*
              *  // Deactivate error messages
              *  // since this is no valid HTML
              *  // we're trying to read from! (@) 
              */
             $finder = new DomXPath($dom);
-            $classname = "post hentry"; // Only get me this class! Its the post itself, only!
-            $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
-            foreach ($nodes as $node) {
-                $content = explode("Anonymous", (string) $node->nodeValue);
-                $content = trim($content[0]);
-                $content = str_replace("", "###CORRECT_ANSWER###", $content);
-                $contentLines = explode("\n", $content);
-                foreach ($contentLines as $line) {
-                    if (!preg_match('/[^\\p{Common}\\p{Latin}]/u', $line)) {
-                        if ($line != "") { //Remove empty strings
-                            if (is_numeric($line)) {
-                                $arrContent[] = $line;
-                            } else {
-                                $arrContent[] = $line;
-                            }
+            $elem = $finder->query('//p | //ul');
+            if ($elem->length <> 0) {
+                for ($i = 0; $i < $elem->length; $i++) {
+                    $element = $elem->item($i);
+                    if ($element->tagName == 'p') {
+                        if (strstr($element->nodeValue, "IT Essentials")) {
+                            $arrContent[$i]["chapters"] = $element->nodeValue;
+                            $arrContent[$i]["question"] = null;
+                            $arrContent[$i]["answers"] = null;
+                        } else {
+                            $arrContent[$i]["question"] = $element->nodeValue;
                         }
+                    } else {
+                        $arrContent[$i]["question"] = null;
                     }
+                    if ($element->tagName == 'ul') {
+                        //is correct answer, append ###CORRECT### to the string
+                        if($element->firstChild == 'span') {
+                            $arrContent[$i]["answers"] = $element->nodeValue."###CORRECT###";
+                        } else {
+                            $arrContent[$i]["answers"] = $element->nodeValue;
+                        }
+                    } else {
+                        $arrContent[$i]["answers"] = null;
+                    }
+//                    if ($element->tagName == 'p') {
+//                        if ($element->nodeValue != "") {
+//                            $arrContent[$i]["question"] = $element->nodeValue;
+//                            $arrContent[$i]["answers"] = null;
+//                        }
+//                        
+//                    } elseif ($element->tagName == 'ul') {
+//                        if ($element->nodeValue != "")
+//                            $arrContent[$i]["answers"] = $element->nodeValue;
+//                    }
                 }
             }
         }
+        unset($arrContent[0]);
         var_dump($arrContent);
+        die();
         ?>
     </body>
 </html>
