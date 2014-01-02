cf425fdd119f8e6b27aa0efce3d921417da3f5fd
Pascal Querner <pascal.querner@mscg.de>
2014-01-02 19:08:59 +0100

    almost...

--- index.php
+++ index.php
@@ -37,74 +37,50 @@
              *  // we're trying to read from! (@) 
              */
             $finder = new DomXPath($dom);
-            $elem = $finder->query('//p | //ul | //span');
+            $elem = $finder->query('//p | //span');
             if ($elem->length <> 0) {
                 $i = 0;
                 foreach ($elem as $ele) {
                     $element = $ele;
-                    //preadd some data, they will later on be thrown away anyway
-                    $arrContent[$currentFile][$i]["question"] = null;
-                    $arrContent[$currentFile][$i]["answers"] = null;
-                    $arrContent[$currentFile][$i]["correctAnswer"] = null;
                     if ($element->tagName == 'p') {
                         if (strstr($element->nodeValue, "IT Essentials")) {
                             $arrContent[$currentFile][$i]["chapters"] = $element->nodeValue;
                         } else {
-                            $arrContent[$currentFile][$i]["question"] = $element->nodeValue;
+                            $firstChars = trim($element->nodeValue);
+                            $firstChars = substr($firstChars, 0, 3);
+                            $firstChars = str_replace(".", "", $firstChars);
+                            $firstChars = trim($firstChars);
+                            //with all this, it must be a question. otherwise I would eat a broomstick!
+                            if (is_numeric($firstChars) && !empty($firstChars)) {
+                                $arrContent[$currentFile][$i + 1]["question"] = $element->nodeValue;
+                            }
                         }
                         $i++;
                     }
-//                    if ($element->tagName == 'ul') {
-//                        if ($element->firstChild == 'li') {
-//                            //TODO: Check if span is inside li. Then its an answer!
-//                            if ($element->hasAttribute('style')) {
-//                                //is answer
-//                                $arrContent[$currentFile][$i]["answers"] = $element->nodeValue;
-//
-//                                //check for correct answer
-//                                $style = $element->getAttribute('style');
-//                                if (stristr($style, "font-family: comic sans ms,sans-serif; font-size: 12px; background-color: #ffff00;")) {
-//                                    //is correct answer
-//                                    $arrContent[$currentFile][$i]["correctAnswer"] = (string) $element->nodeValue;
-////                                var_dump($element->nodeValue);
-//                                }
-//                            }
-//                        }
-//                    }
-//                    TODO: 1 question can have more than one answer! Fix that!
                     if ($element->tagName == 'span') {
                         if ($element->hasAttribute('style')) {
-                            //is answer
-//                            var_dump($element->nodeValue);
-                            $arrContent[$currentFile][$i]["answers"] = $element->nodeValue;
-                            $style = $element->getAttribute('style');
-                            if (stristr($style, "font-family: comic sans ms,sans-serif; font-size: 12px; background-color: #ffff00;")) {
-                                //is correct answer
-                                $arrContent[$currentFile][$i]["correctAnswer"] = (string) $element->nodeValue;
-//                                var_dump($element->nodeValue);
+                            if (!empty($element->nodeValue)) {
+                                if (!stristr($element->nodeValue, "?")) {
+                                    $style = $element->getAttribute('style');
+                                    if (stristr($style, "font-family: comic sans ms,sans-serif; font-size: 12px; background-color: #ffff00;")) {
+                                        //is correct answer
+                                        $arrContent[$currentFile][$i]["correctAnswer"][] = (string) $element->nodeValue;
+                                    }
+                                    if ($element->nodeValue === $curVal) {
+                                        $arrContent[$currentFile][$i]["answers"][] = (string) $element->nodeValue;
+                                    }
+                                    $curVal = (string) $element->nodeValue;
+                                }
                             }
+
+
                         }
                     }
                 }
             }
+            var_dump($arrContent);
+            die();
         }
-        //remove empty values from array, as they are unneded
-        //yes, i hate this 'function'
-        foreach ($arrContent as $contentOuterKey => $contentOuterValue) {
-            foreach ($contentOuterValue as $contentKey => $contentValue) {
-                foreach ($contentValue as $key => $val) {
-                    //first remove empty value
-                    if (empty($val)) {
-                        unset($arrContent[$contentOuterKey][$contentKey][$key]);
-                    }
-                    //check if complete key is empty, if so, remove it from array aswell
-                    if (empty($arrContent[$contentOuterKey][$contentKey])) {
-                        unset($arrContent[$contentOuterKey][$contentKey]);
-                    }
-                }
-            }
-        }
-        //reindex array
         var_dump($arrContent);
         ?>
     </body>
