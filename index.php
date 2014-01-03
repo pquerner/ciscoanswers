130688a9edeeadd8b54bb55e7100a3b99828c3cf
Pascal Querner <pascal.querner@mscg.de>
2014-01-03 02:34:04 +0100

    v0.1 (working with some flaws!)

--- index.php
+++ index.php
@@ -15,6 +15,80 @@
             return preg_replace("/\\.[^.\\s]{3,4}$/", "", $string);
         }
 
+        function buildPage($arr) {
+            if (isset($_POST) && !empty($_POST)) {
+                if ($_POST['exam'] === 'noval')
+                    die('Du musst schon was auswählen!');
+                if (is_array($arr[$_POST['exam']])) {
+                    foreach ($arr[$_POST['exam']] as $aKey => $aVal) {
+                        if (is_array($aVal)) {
+                            foreach ($aVal as $bKey => $bVal) {
+                                if ($bKey === 'chapter') {
+                                    echo "<b>" . $bVal . "</b><br /><br />";
+                                }
+                                if ($bKey === 'question') {
+                                    echo "<br /><i>" . $bVal . "</i><br /><br />";
+                                }
+                                //TODO: Return random value (either correct answer or a normal answer)
+                                if ($bKey === 'correctAnwer') {
+                                    shuffle($bVal);
+                                    foreach ($bVal as $cKey => $cVal) {
+                                        echo $cVal . "<br />";
+                                    }
+                                }
+                                if ($bKey === 'answers') {
+                                    shuffle($bVal);
+                                    foreach ($bVal as $cKey => $cVal) {
+                                        echo $cVal . "<br />";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        function removeDoubleAnswers($arr) {
+            //remove double answers
+            foreach ($arr as $contentOuterKey => $contentOuterValue) {
+                foreach ($contentOuterValue as $contentKey => $contentValue) {
+                    foreach ($contentValue as $key => $val) {
+                        if (is_array($val)) {
+                            $cV = "";
+                            foreach ($val as $k => $v) {
+                                if ($v === $cV) {
+                                    unset($arr[$contentOuterKey][$contentKey][$key][$k]);
+                                }
+                                $cV = $v;
+                            }
+                        }
+                    }
+                }
+            }
+            return $arr;
+        }
+
+        function buildSelectForm($arr) {
+            echo '<form action="index.php" method="POST">';
+            echo '<select onchange="this.form.submit()" name="exam">';
+            echo "<option selected value='noval'>Bitte auswählen</option>";
+            foreach ($arr as $aKey => $aVal) {
+                if (isset($_POST) && !empty($_POST)) {
+                    if ($_POST['exam']) {
+                        if ($aKey === $_POST['exam']) {
+                            echo "<option selected value='" . $aKey . "'>" . $aKey . "</option>";
+                        } else {
+                            echo "<option value='" . $aKey . "'>" . $aKey . "</option>";
+                        }
+                    }
+                }
+            }
+            echo "</select>";
+            echo '<input type="submit" value="Absenden">';
+            echo "</form>";
+        }
+
         // Get all the HTML files from within IT Essentials only!
         $filetypes = array("html");
         $path = './ciscoanswers/ccna.smarthand.ro/';
@@ -44,7 +118,7 @@
                     $element = $ele;
                     if ($element->tagName == 'p') {
                         if (strstr($element->nodeValue, "IT Essentials")) {
-                            $arrContent[$currentFile][$i]["chapters"] = $element->nodeValue;
+                            $arrContent[$currentFile][$i]["chapter"] = $element->nodeValue;
                         } else {
                             $firstChars = trim($element->nodeValue);
                             $firstChars = substr($firstChars, 0, 3);
@@ -62,21 +136,23 @@
                             if (!empty($element->nodeValue)) {
                                 if (!strpos($element->nodeValue, "?")) {
                                     $style = $element->getAttribute('style');
-                                    if (stristr($style, "font-family: comic sans ms,sans-serif; font-size: 12px; background-color: #ffff00;")) {
+                                    if (stristr($style, "font-family: comic sans ms,sans-serif; font-size: 12px; background-color: #ffff00;") || stristr($style, "background-color: #ffff00;")) {
                                         //is correct answer
                                         $arrContent[$currentFile][$i]["correctAnswer"][] = (string) $element->nodeValue;
                                     }
-                                    $arrContent[$currentFile][$i]["answers"][] = (string) $element->nodeValue;
+                                    if (!strpos($element->nodeValue, "IT Essentials")) {
+                                        $arrContent[$currentFile][$i]["answers"][] = $element->nodeValue;
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
-            var_dump($arrContent);
-            die();
         }
-        var_dump($arrContent);
+        $arrContent = removeDoubleAnswers($arrContent);
+        buildSelectForm($arrContent);
+        buildPage($arrContent);
         ?>
     </body>
 </html>
